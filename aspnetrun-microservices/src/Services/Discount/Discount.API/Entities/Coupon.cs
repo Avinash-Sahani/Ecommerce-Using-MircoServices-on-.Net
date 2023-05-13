b1f76f05aca3aaae95199a509a47ec267e1cbdf0
Avinash Sahani <avinashsahani66@gmail.com>
2023-05-14 02:20:34 +0500

    ✨(Enities-> Coupon) : Added Entity Class Coupon

---
+++ aspnetrun-microservices/src/Services/Discount/Discount.API/Entities/Coupon.cs
@@ -0,0 +1,12 @@
+namespace Discount.API.Entities;
+
+public class Coupon
+{
+    public int Id { get; set; }
+
+    public string ProductName { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public int Amount { get; set; } = 0;
+}

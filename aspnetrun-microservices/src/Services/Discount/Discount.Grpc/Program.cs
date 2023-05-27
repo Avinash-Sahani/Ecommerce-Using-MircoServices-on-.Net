5bd903250a1bc72859dd74d3b94aa4125d3672cf
Avinash Sahani <avinashsahani66@gmail.com>
2023-05-28 03:52:23 +0500

    🚧(DiscountService): intial implmentation of Discount.Service

--- aspnetrun-microservices/src/Services/Discount/Discount.Grpc/Program.cs
+++ aspnetrun-microservices/src/Services/Discount/Discount.Grpc/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddScoped<IDbConnection, DatabaseConnection>();
 var app = builder.Build();
 app.MigrateDatabase<Program>();
 // Configure the HTTP request pipeline.
-
+app.MapGrpcService<DiscountService>();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
--- aspnetrun-microservices/src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ aspnetrun-microservices/src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,6 +1,41 @@
+using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+
 namespace Discount.Grpc.Services;
 
-public class DiscountService
+public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
 {
+    private IDiscountRepository DiscountRepository { get; }
+    private ILogger<DiscountService> Logger { get; }
     
+    public DiscountService(IDiscountRepository discountRepository, ILogger<DiscountService> logger)
+    {
+        DiscountRepository = discountRepository ?? throw new ArgumentNullException(nameof(discountRepository));
+        Logger = logger ?? throw new ArgumentNullException(nameof(logger));;
+    }
+
+    public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        return base.CreateDiscount(request, context);
+    }
+
+    public async override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await DiscountRepository.GetDiscount(request.ProductName);
+        if (coupon.Id == -1)
+            throw new RpcException(
+                new Status(StatusCode.NotFound, $"Discount Code for {request.ProductName} not found"));
+        return coupon;
+    }
+
+    public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        return base.DeleteDiscount(request, context);
+    }
+
+    public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        return base.UpdateDiscount(request, context);
+    }
 }

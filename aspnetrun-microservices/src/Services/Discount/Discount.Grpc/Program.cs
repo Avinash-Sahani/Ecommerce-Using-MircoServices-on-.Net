08caee06f7214ca870599ead7175f77e7d973b1d
Avinash Sahani <avinashsahani66@gmail.com>
2023-05-28 03:23:08 +0500

    🚧(discount.proto) : Wrote my first ever proto file, discount.proto

--- aspnetrun-microservices/src/Services/Discount/Discount.Grpc/Program.cs
+++ aspnetrun-microservices/src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,3 +1,6 @@
+using Discount.Grpc;
+using Discount.Grpc.Data;
+using Discount.Grpc.Repositories;
 using Discount.Grpc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,11 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddScoped<IDbConnection, DatabaseConnection>();
 
 var app = builder.Build();
-
+app.MigrateDatabase<Program>();
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");

87b08b34f6c487a633ba11fd55df219ec34a7aca
Avinash Sahani <avinashsahani66@gmail.com>
2023-06-04 03:19:14 +0500

    ✨(IEmailService,Email,EmailSettings) : Added classes and interfaces for email

---
+++ aspnetrun-microservices/src/Services/Ordering/Ordering.Application/Models/Email.cs
@@ -0,0 +1,9 @@
+namespace Ordering.Application.Models;
+#nullable enable
+public class Email
+{
+    public string? To { get; set; }
+    public string? Subject { get; set; }
+    public string? Body { get; set; }
+    
+}

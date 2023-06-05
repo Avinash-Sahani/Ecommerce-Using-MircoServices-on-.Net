f40514bf4b1d724e01a1c99956dacd640f73ed6f
Avinash Sahani <avinashsahani66@gmail.com>
2023-06-06 02:20:25 +0500

    🚧(UnhandledExcpetionBehavour) : Add one more behaviour

---
+++ aspnetrun-microservices/src/Services/Ordering/Ordering.Application/Behaviours/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,27 @@
+using Microsoft.Extensions.Logging;
+using MediatR;
+namespace Ordering.Application.Behaviours;
+
+public class UnhandledExceptionBehaviour<TRequest,TResponse> : IPipelineBehavior<TRequest,TResponse>
+{
+    private readonly ILogger<TRequest> _logger;
+
+    public UnhandledExceptionBehaviour(ILogger<TRequest> logger)
+    {
+        _logger = logger;
+    }
+
+    public  async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        try
+        {
+            return await next();
+        }
+        catch (Exception e)
+        {
+            var requestName = typeof(TRequest).Name;
+            _logger.LogError(e,"Application Request : Unhandled Exception for Request {Name} {@Request}");
+            throw;
+        }
+    }
+}

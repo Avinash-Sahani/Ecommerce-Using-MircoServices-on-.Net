4c7f1170c8a663eca16b52cb297d906066aa2f16
Avinash Sahani <avinashsahani66@gmail.com>
2023-06-06 02:47:55 +0500

    ➕(Ordering.Application) :Added Depencdcy Injection in Startup Class

---
+++ aspnetrun-microservices/src/Services/Ordering/Ordering.Application/ApplicationServiceRegistraion.cs
@@ -0,0 +1,22 @@
+using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+
+namespace Ordering.Application;
+
+public static class ApplicationServiceRegistraion
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+    
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+        return services;
+
+    }
+}

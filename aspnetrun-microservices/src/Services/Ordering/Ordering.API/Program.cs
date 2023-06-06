a0ded1d49b851b7a4b5d741bc74b8113be12a85b
Avinash Sahani <avinashsahani66@gmail.com>
2023-06-07 04:26:00 +0500

    ✨(Program.CS) : Added Application and Infrastructure layer dependices in Presentation Layer

--- aspnetrun-microservices/src/Services/Ordering/Ordering.API/Program.cs
+++ aspnetrun-microservices/src/Services/Ordering/Ordering.API/Program.cs
@@ -1,4 +1,5 @@
 using Ordering.Application;
+using Ordering.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +10,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddApplicationServices();
-
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

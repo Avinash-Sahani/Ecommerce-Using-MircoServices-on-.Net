ff6477690658dc8f05684e61a61687b3bbe25f02
Avinash Sahani <avinashsahani66@gmail.com>
2023-07-26 02:23:06 +0500

    🚧(Program,OcelotApiGw) : Added Ocelot service and logging

--- aspnetrun-microservices/src/ApiGateways/OcelotApiGw/Program.cs
+++ aspnetrun-microservices/src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,5 +1,16 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+builder.Services.AddOcelot();
+
 var app = builder.Build();
+await app.UseOcelot();
+
 
 app.MapGet("/", () => "Hello World!");
 

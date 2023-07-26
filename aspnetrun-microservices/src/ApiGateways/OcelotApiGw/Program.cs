0ae648b0f0e9d9ae730791c835207b6b42396c92
Avinash Sahani <avinashsahani66@gmail.com>
2023-07-26 14:40:30 +0500

    🔧(ocelot.Local) : Add configurations for Apis in ocelot local enivroment

--- aspnetrun-microservices/src/ApiGateways/OcelotApiGw/Program.cs
+++ aspnetrun-microservices/src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,14 +1,18 @@
+using Microsoft.Extensions.Hosting.Internal;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using HostingEnvironmentExtensions = Microsoft.AspNetCore.Hosting.HostingEnvironmentExtensions;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var currentEnvironmentName =  builder.Environment.EnvironmentName;;
+builder.Configuration.AddJsonFile($"ocelot.{currentEnvironmentName}.json", optional: true, reloadOnChange: true);
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
 builder.Logging.AddConsole();
 builder.Logging.AddDebug();
 builder.Services.AddOcelot();
 
 var app = builder.Build();
+
 await app.UseOcelot();
 
 

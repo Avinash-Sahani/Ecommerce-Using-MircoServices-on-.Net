8a3d593f074de45a21f5c71002058b1553c860df
Avinash Sahani <avinashsahani66@gmail.com>
2023-07-28 02:33:06 +0500

    🚧(Program.cs) : Added services in startup

--- aspnetrun-microservices/src/ApiGateways/Shopping.Aggregator/Shopping.Aggregator/Program.cs
+++ aspnetrun-microservices/src/ApiGateways/Shopping.Aggregator/Shopping.Aggregator/Program.cs
@@ -1,3 +1,5 @@
+using Shopping.Aggregator.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var configuration = builder.Configuration;
+builder.Services.AddHttpClient<ICatalogService,CatalogService>(service =>
+    service.BaseAddress = new Uri(configuration["ApiSettings:CatalogUrl"] ?? string.Empty));
+builder.Services.AddHttpClient<IBasketService,BasketService>(service =>
+    service.BaseAddress = new Uri(configuration["ApiSettings:BasketUrl"] ?? string.Empty));
+builder.Services.AddHttpClient<IOrderService,OrderService>(service =>
+    service.BaseAddress = new Uri(configuration["ApiSettings:OrderingUrl"] ?? string.Empty));
 
 var app = builder.Build();
 
